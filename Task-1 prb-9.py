48ad021550be19d43f8acb3030e9b144b1e29628
indhuchandrasekar <156568683+indhuchandrasekar@users.noreply.github.com>
2024-01-19 18:51:12 +0530

    Update Task-1 prb-9.py

--- Task-1 prb-9.py
+++ Task-1 prb-9.py
@@ -1,11 +1,17 @@
-string=raw_input("Enter string:")
-char=0
-word=1
-for i in string:
-      char=char+1
-      if(i==' '):
-            word=word+1
-print("Number of words in the string:")
-print(word)
-print("Number of characters in the string:")
-print(char)
+# Problem 9 : Program That Takes A String And Returns Number of Words In It.
+# to count words in string
+# using split()
+ 
+# initializing string
+test_string = "Welcome To GUVI"
+ 
+ 
+# printing original string
+print("The original string is : " + test_string)
+ 
+# using split()
+# to count words in string
+res = test_string.count(" ")+1
+ 
+# printing result
+print("The number of words in string are : " + str(res))
